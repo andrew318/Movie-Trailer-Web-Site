4bdeecfb4fb1a68159015b8840042a8096cb7e4c
Andrew Schumacher <andrew-s18@hotmail.com>
2015-03-10 16:08:59 -0500

    Committed by Udacity

---
+++ Movies/Video.py
@@ -0,0 +1,10 @@
+class Video():
+
+	"""This class defines information for generic videos."""
+
+	def __init__(self, title, duration):
+
+		print("Video constructor called.")
+
+		self.title = title
+		self.duration = duration
